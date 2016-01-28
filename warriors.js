7a497fd485bc5e89a8c5c07777707fbb1b2cf924
Phil Rajchgot <tophil@outlook.com>
2016-01-28 23:13:34 +0000

    Changing to an external prototype object which contains the methods for warriors

--- warriors.js
+++ warriors.js
@@ -8,25 +8,12 @@ In addition to that, every object that gets created should have the following me
 fight: will output to the console: " rushes to the arena with "
 faceoff: faceoff takes one argument called opponent. Based on the power of each opponent, this method should output to the console which player won the fight based on their power. Be as creative as you like with the text of this method :)
 Finally, create a bunch of warriors and make them fight together.*/
-function Warrior (name, gender) {
-    this.name = name;
-    
-    if (gender === "M" || gender === "F") {
-        this.gender = gender;
-    }
-    else {
-        throw new Error ("I only understand 'M' or 'F', sorry!");
-    }
-    
-    this.level = 1;
-    this.weapon = "wooden sword";
-    this.power = Math.floor( Math.random() * 100 ) + 1;
-    
-    this.fight = function (opponent) {
+var warriorPrototype = {
+    fight: function (opponent) {
         console.log(this.name + " rushes to the arena with " + opponent.name + "...");
-    }
+    },
     
-    this.faceoff = function (opponent) {
+    faceoff: function (opponent) {
         console.log(this.name + " hits " + opponent.name + " in the face with a " + this.weapon + "!");
         console.log(this.name + " takes a knee to the stomach from " + opponent.name + "!");
         console.log(this.name + " blocks an attack by " + opponent.name + "!");
@@ -41,8 +28,27 @@ function Warrior (name, gender) {
         else {
             console.log(this.name + " loses the fight and falls apart. Disgusting. " + opponent.name + " is the victor.");
         }
-    }
+    }   
 }
+
+
+
+function Warrior (name, gender) {
+    this.name = name;
+    
+    if (gender === "M" || gender === "F") {
+        this.gender = gender;
+    }
+    else {
+        throw new Error ("I only understand 'M' or 'F', sorry!");
+    }
+    
+    this.level = 1;
+    this.weapon = "wooden sword";
+    this.power = Math.floor( Math.random() * 100 ) + 1;
+}
+
+Warrior.prototype = warriorPrototype;
 
 var warrior1 = new Warrior("Isaac", "M");
 var warrior2 = new Warrior("Mary", "F");
